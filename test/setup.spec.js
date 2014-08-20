db5befaf9a4dc5449d25f073eb362de3c092f8db
Kyle Burnett <kburnett@dotdecimal.com>
2014-08-20 10:31:19 -0400

    Corrected documentation and ec2 file structure

--- test/setup.spec.js
+++ test/setup.spec.js
@@ -6,7 +6,23 @@
  * Copyright:  (c) 2014 .decimal, Inc. All rights reserved.
  */
 
+var Promise = require('bluebird');
+var fs = require('fs');
 var path = require('path');
+
+Promise.promisifyAll(fs);
+
+function removeDir(dir) {
+    return fs.readdirAsync(dir).each(function(file) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            return removeDir(path.join(dir, file));
+        } else {
+            return fs.unlinkAsync(path.join(dir, file));
+        }
+    }).then(function() {
+        return fs.rmdirAsync(dir);
+    });
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Before
@@ -19,9 +35,13 @@ before(function() {
     var AWSLocal = require('../lib/server');
     global.awsLocal = new AWSLocal(path.resolve('./test_root'));
 
-    return awsLocal.listen(4242);
+    return fs.mkdirAsync(path.resolve('./test_root')).then(function() {
+        return awsLocal.listen(4242);
+    });
 });
 
 after(function() {
-    return awsLocal.close();
+    return removeDir(path.resolve('./test_root')).then(function() {
+        return awsLocal.close();
+    });
 });
